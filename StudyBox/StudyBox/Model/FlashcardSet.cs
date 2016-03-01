2ca9018218a6a2bf73ff5ec98216d1a8c675fca7
mateusz-szczepanski <mateusz.szczepanski.1992@gmail.com>
2016-03-01 19:02:39 +0100

    Added temporary model files (based on apiary) and service to deserialize json

---
+++ StudyBox/StudyBox/Model/FlashcardSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyBox.Model
+{
+    public class FlashcardSet
+    {
+        public string id;
+        public string name;
+    }
+}
---
+++ StudyBox/StudyBox/Services/DeserializeJsonService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StudyBox.Model;
+using System.Diagnostics;
+
+namespace StudyBox.Services
+{
+    public static class DeserializeJsonService
+    {
+        public static Flashcard GetFlashCardFromJson(string jsonToDeserialize)
+        {
+            if (jsonToDeserialize != null && jsonToDeserialize != "")
+            {
+                try
+                {
+                    Flashcard flashCardObject = JsonConvert.DeserializeObject<Flashcard>(jsonToDeserialize);
+                    return flashCardObject;
+                }
+                catch(JsonSerializationException ex)
+                {
+                    Debug.WriteLine("JsonSerializationException - " + ex.Message);
+                }
+                catch(JsonException ex)
+                {
+                    Debug.WriteLine("JsonException - " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Other Exception - " + ex.Message);
+                }
+                return null;
+                
+            }
+            else
+                return null;
+        }
+
+        public static List<Flashcard> GetFlashCardsFromJson(string jsonToDeserialize)
+        {
+            if (jsonToDeserialize != null && jsonToDeserialize != "")
+            {
+                try
+                {
+                    List<Flashcard> flashCardObject = JsonConvert.DeserializeObject<List<Flashcard>>(jsonToDeserialize);
+                    return flashCardObject;
+                }
+                catch (JsonSerializationException ex)
+                {
+                    Debug.WriteLine("JsonSerializationException - " + ex.Message);
+                }
+                catch (JsonException ex)
+                {
+                    Debug.WriteLine("JsonException - " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Other Exception - " + ex.Message);
+                }
+                return null;
+
+            }
+            else
+                return null;
+        }
+
+        public static List<FlashcardSet> GetFlashCardSetFromJson(string jsonToDeserialize)
+        {
+            if (jsonToDeserialize != null && jsonToDeserialize != "")
+            {
+                try
+                {
+                    List<FlashcardSet> flashCardSetObject = JsonConvert.DeserializeObject<List<FlashcardSet>>(jsonToDeserialize);
+                    return flashCardSetObject;
+                }
+                catch (JsonSerializationException ex)
+                {
+                    Debug.WriteLine("JsonSerializationException - " + ex.Message);
+                }
+                catch (JsonException ex)
+                {
+                    Debug.WriteLine("JsonException - " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Other Exception - " + ex.Message);
+                }
+                return null;
+
+            }
+            else
+                return null;
+        }
+    }
+}
