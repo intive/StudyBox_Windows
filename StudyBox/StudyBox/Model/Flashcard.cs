7cde2b955245da6a148aecbabe053bab936f449c
Konrad <oporski.konrad@gmail.com>
2016-03-02 19:18:13 +0100

    flashcard model

---
+++ StudyBox/StudyBox/Model/Flashcard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyBox.Model
+{
+    public class Flashcard
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Flashcard() { }
+        public Flashcard(string id, string question, string answer)
+        {
+            Id = id;
+            Question = question;
+            Answer = answer;
+        }
+    }
+}
