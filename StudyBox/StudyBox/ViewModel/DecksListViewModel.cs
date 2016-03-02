27800d59ceed82823736f91c47faf2715b5de407
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2016-03-02 14:18:05 +0100

    Added: NavigationService, Improved: Adaptive Triggers

--- StudyBox/StudyBox/ViewModel/DecksListViewModel.cs
+++ StudyBox/StudyBox/ViewModel/DecksListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,33 @@ namespace StudyBox.ViewModel
 {
     public class DecksListViewModel : ViewModelBase
     {
+        //Collection TO TESTS!
+        private ObservableCollection<int> _funnyIntsCollection = new ObservableCollection<int>();
+
+        public DecksListViewModel()
+        {
+            FunnyIntsCollection=new ObservableCollection<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                FunnyIntsCollection.Add(i);
+            }
+        }
+
+
+        public ObservableCollection<int> FunnyIntsCollection
+        {
+            get
+            {
+                return _funnyIntsCollection;
+            }
+            set
+            {
+                if (_funnyIntsCollection != value)
+                {
+                    _funnyIntsCollection = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
     }
 }
