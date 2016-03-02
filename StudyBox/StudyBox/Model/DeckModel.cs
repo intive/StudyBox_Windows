945016cf6acf0766d1c8c08c229b5365176aeb00
Krzysztof Młynarski <kmlynarski@wi.zut.edu.pl>
2016-03-02 21:39:46 +0100

    Added structure Deck

---
+++ StudyBox/StudyBox/Model/DeckModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyBox.Model
+{
+    public class DeckModel
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+
+        public DeckModel() { }
+
+        public DeckModel(string id)
+        {
+            this.ID = id;
+        }
+
+        public DeckModel(string id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+    }
+}
