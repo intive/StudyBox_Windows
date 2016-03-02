96b2684f24bedac5cf5174406b02b1fbd34c2179
Konrad <oporski.konrad@gmail.com>
2016-03-02 23:33:06 +0100

    added Deck property

--- StudyBox/StudyBox/Model/Flashcard.cs
+++ StudyBox/StudyBox/Model/Flashcard.cs
@@ -9,14 +9,16 @@ namespace StudyBox.Model
     public class Flashcard
     {
         public string Id { get; set; }
+        public Deck Deck { get; set; }
         public string Question { get; set; }
         public string Answer { get; set; }
         public string Hint { get; set; }
 
         public Flashcard() { }
-        public Flashcard(string id, string question, string answer, string hint)
+        public Flashcard(string id, Deck deck, string question, string answer, string hint)
         {
             Id = id;
+            Deck = deck;
             Question = question;
             Answer = answer;
             Hint = hint;
