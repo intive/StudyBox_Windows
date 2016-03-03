6b739249246e128513eeb01151bde5d2fe57f96c
bobista0 <borys.rybak@gmail.com>
2016-03-04 00:10:03 +0100

    exclude assets folder and add service test classes

---
+++ StudyBox/StudyBoxTest/Services/RestServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace StudyBoxTest
+{
+    [TestClass]
+    public class RestServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
