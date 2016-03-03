946d22b1ef9958ff4a93bb389a1c12cc47379770
bobista0 <borys.rybak@gmail.com>
2016-03-03 23:30:35 +0100

    added proper unit test app project for windows universal

---
+++ StudyBox/StudyBoxTest/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace StudyBoxTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
