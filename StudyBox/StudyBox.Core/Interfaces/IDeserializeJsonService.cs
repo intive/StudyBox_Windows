2c48a63522c8d6c9ac90d6edd91ef40532ec780d
bobista0 <borys.rybak@gmail.com>
2016-03-09 13:16:45 +0100

    delete class library universal windows project

---
+++ StudyBox/StudyBox.Core/Interfaces/IDeserializeJsonService.cs
@@ -0,0 +1,13 @@
+﻿using StudyBox.Core.Models;
+using System.Collections.Generic;
+
+namespace StudyBox.Core.Interfaces
+{
+    public interface IDeserializeJsonService
+    {
+        Flashcard GetFlashcardFromJson(string jsonToDeserialize);
+        List<Flashcard> GetFlashcardsFromJson(string jsonToDeserialize);
+        Deck GetDeckFromJson(string jsonToDeserialize);
+        List<Deck> GetDecksFromJson(string jsonToDeserialize);
+    }
+}
