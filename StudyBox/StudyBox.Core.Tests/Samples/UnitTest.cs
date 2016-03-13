9863752ed8f5592b097da64a518775638fd2bcfe
bobista0 <borys.rybak@gmail.com>
2016-03-14 00:25:07 +0100

    add sample unit test file

---
+++ StudyBox/StudyBox.Core.Tests/Samples/UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace StudyBox.Core.Tests.Samples
+{
+    [TestClass]
+    public class When_adding_two_numbers
+    {
+        [TestMethod]
+        public void the_add_should_returns_sum_of_passed_integers()
+        {
+            //Arrange
+            var firstNumber = 5;
+            var secondNumber = 4;
+
+            //Act
+            var result = Add(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(9, result);
+            Assert.AreNotEqual(8, result);
+        }
+
+        private int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
