233c2c3901ccc37970e1f96c3ddcc8765cbfa7ca
bobista0 <borys.rybak@gmail.com>
2016-03-13 16:25:58 +0100

    clear unused usings and delete redundant comments

--- StudyBox/StudyBox/App.xaml.cs
+++ StudyBox/StudyBox/App.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using StudyBox.View;
 
@@ -53,11 +43,8 @@ namespace StudyBox
 
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
@@ -67,19 +54,14 @@ namespace StudyBox
                     //TODO: Load state from previously suspended application
                 }
 
-                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
             if (rootFrame.Content == null)
             {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                //rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 rootFrame.Navigate(typeof(DecksListView), e.Arguments);
             }
-            // Ensure the current window is active
+
             Window.Current.Activate();
         }
 
