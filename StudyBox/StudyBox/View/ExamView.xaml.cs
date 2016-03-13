7e83be8601dedd723a091543de88bd44f14cc92c
bobista0 <borys.rybak@gmail.com>
2016-03-13 16:21:58 +0100

    clear unused usings

--- StudyBox/StudyBox/View/ExamView.xaml.cs
+++ StudyBox/StudyBox/View/ExamView.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using GalaSoft.MvvmLight.Messaging;
-using StudyBox.Messages;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using StudyBox.Core.Messages;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
