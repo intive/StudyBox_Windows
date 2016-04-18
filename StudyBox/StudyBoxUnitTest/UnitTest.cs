2150f5c01d09e428749503c7a6ab4a7f3262471b
Krzysztof Młynarski <kmlynarski@wi.zut.edu.pl>
2016-04-18 18:41:35 +0200

    Added the project to unit test

---
+++ StudyBox/StudyBoxUnitTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace StudyBoxUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
