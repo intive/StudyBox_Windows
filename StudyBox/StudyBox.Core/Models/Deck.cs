c73e58315f3686fbda1f669e65078bc2ae50559f
Lukasz Sielewicz <lsielewicz@wi.zut.edu.pl>
2016-04-20 21:56:23 +0200

    Added: CountOfFlashcards property in Deck model and DeckListView (below the deck name)

--- StudyBox/StudyBox.Core/Models/Deck.cs
+++ StudyBox/StudyBox.Core/Models/Deck.cs
@@ -13,17 +13,32 @@ namespace StudyBox.Core.Models
         [JsonProperty(PropertyName = "isPublic")]
         public bool IsPublic { get; private set; }
 
-        public Deck() { }
+        //TODO connect with background as soon as will be possible
+        public int CountOfFlashcards { get; set; }
+
+        public Deck()
+        {
+            this.CountOfFlashcards = 0;
+        }
 
         public Deck(string id)
         {
             this.ID = id;
+            this.CountOfFlashcards = 0;
         }
 
         public Deck(string id, string name)
         {
             this.ID = id;
             this.Name = name;
+            this.CountOfFlashcards = 0;
+        }
+
+        public Deck(string id, string name, int countOfFlashCards)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.CountOfFlashcards = countOfFlashCards;
         }
     }
 }
